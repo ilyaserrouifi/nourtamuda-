a6e9fdfc50a1ccb00ea0612a40d09be4ab599aae
iliass errouifi <ilyaserrouifi@gmail.com>
2026-06-15 19:48:53 +0100

    Update config.php

--- php/config.php
+++ php/config.php
@@ -20,7 +20,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
-// Activer les erreurs pour le développement (à désactiver en production)
+// Activer les erreurs pour le développement
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
